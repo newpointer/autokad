b08b942bf102d4498d391f53821a501b11a80a4d
ankostyuk <andrew.kostyuk@gmail.com>
2015-01-13 02:08:50 +0300

    Постраничные запросы к kad.arbitr.ru. Рефакторинг. Стилизация.

--- src/autokad/autokad-resource.js
+++ src/autokad/autokad-resource.js
@@ -7,56 +7,54 @@ define(function(require) {'use strict';
 
                   require('underscore');
     var angular = require('angular');
-
                   require('resource');
 
     return angular.module('np.autokad-resource', ['np.resource'])
         //
         .constant('npAutokadResourceConfig', {
-            resource: {
-                // 'kad.search.url': '/autokad/data/demo/r.json',
-                'kad.search.url': '/kad.arbitr.ru/Kad/SearchInstances'
+            'kad.search': {
+                // url: '/autokad/data/demo/r.json',
+                url: '/kad.arbitr.ru/Kad/SearchInstances'
             }
         })
         //
         .factory('npAutokadResource', ['$log', '$q', '$http', 'npAutokadResourceConfig', 'npResource', function($log, $q, $http, npAutokadResourceConfig, npResource){
 
-            var config = npAutokadResourceConfig.resource || {};
+            var kadSearchConfig = npAutokadResourceConfig['kad.search'];
 
             // API
             return {
-                // docs/autokad.md#%D0%9F%D0%BE%D0%B8%D1%81%D0%BA-%D0%B0%D1%80%D0%B1%D0%B8%D1%82%D1%80%D0%B0%D0%B6%D0%BD%D1%8B%D1%85-%D0%B4%D0%B5%D0%BB
                 kadSearch: function(options) {
                     return npResource.request({
+                        // docs/autokad.md#%D0%9F%D0%BE%D0%B8%D1%81%D0%BA-%D0%B0%D1%80%D0%B1%D0%B8%D1%82%D1%80%D0%B0%D0%B6%D0%BD%D1%8B%D1%85-%D0%B4%D0%B5%D0%BB
                         // method: 'GET',
                         method: 'POST',
-                        url: config['kad.search.url'],
-                        data: {
+                        url: kadSearchConfig.url,
+                        data: _.extend({}, kadSearchConfig.defaultRequestData, {
                             "Sides": [{
                                 "Name": options.r.q,
                                 "Type": -1,
                                 "ExactMatch": false
                             }],
-                            "DateFrom": options.r.dateFrom || null,
-                            "DateTo": options.r.dateTo || null,
-                            "Page": options.r.page || 1,
-                            "Count": options.r.pageSize || 25,
-
+                            "DateFrom": options.r.dateFrom,
+                            "DateTo": options.r.dateTo,
+                            "Page": options.r.page,
+                            "Count": 25,
                             "Courts": [],
                             "Judges": [],
                             "CaseNumbers": [],
                             "WithVKSInstances": false
-                        },
+                        }),
                         headers: {
                             "Accept": 'application/json, text/javascript, */*',
                             "Content-Type": 'application/json',
                             "x-date-format": 'iso',
                             "X-Requested-With": 'XMLHttpRequest'
                             // Установить посредством веб-сервера (см. документацию):
-                            //  Cookie: ?
-                            //  Host: ?
-                            //  Origin: ?
-                            //  Referer: ?
+                            //  Cookie: *
+                            //  Host: *
+                            //  Origin: *
+                            //  Referer: *
                             // Например, nginx...
                             //  location /kad.arbitr.ru/Kad/ {
                             //      proxy_pass http://kad.arbitr.ru/Kad/;
--- src/autokad/autokad.js
+++ src/autokad/autokad.js
@@ -14,109 +14,160 @@ define(function(require) {'use strict';
     var i18n            = require('i18n'),
         angular         = require('angular'),
         moment          = require('moment');
+                          require('ng-infinite-scroll');
 
     var submodules = {
         autokadResource:    require('./autokad-resource')
     };
 
     //
-    var DEFAULT_SEARCH_PARAMS = {
-        name: null,
-        ogrn: null,
-        inn: null,
-        source: 'name',
-        period: 'all',
-    };
-
-    var DEFAULT_REQUEST = {
-        q: null,
-        dateFrom: null,
-        dateTo: null,
-        page: 1,
-        pageSize: 25
-    };
-
-    var period = new Period();
-
-    var TRANSFORM_SEARCH_PARAMS = {
-        'source': {
-            'name': function(searchParams, request) {
-                request['q'] = searchParams['name'];
-            },
-            'ogrn': function(searchParams, request) {
-                request['q'] = searchParams['ogrn'];
-            },
-            'inn': function(searchParams, request) {
-                request['q'] = searchParams['inn'];
-            }
-        },
-        'period': {
-            'all': function(searchParams, request) {
-                request['dateFrom'] = null;
-            },
-            'year': function(searchParams, request) {
-                request['dateFrom'] = period.isoYearAgo;
-            },
-            'month': function(searchParams, request) {
-                request['dateFrom'] = period.isoMonthAgo;
-            }
-        }
-    };
-
-    function buildRequest(searchParams) {
-        var request = DEFAULT_REQUEST,
-            value;
-
-        _.each(TRANSFORM_SEARCH_PARAMS, function(paramTransform, param){
-            value = searchParams[param];
-            paramTransform[value](searchParams, request);
-        });
-
-        return request;
-    }
-
-    function Period() {
-        var startOfDay  = moment().startOf('day'),
-            monthAgo    = moment(startOfDay).subtract(1, 'months'),
-            yearAgo     = moment(startOfDay).subtract(1, 'years'),
-            isoMonthAgo = toISO(monthAgo),
-            isoYearAgo  = toISO(yearAgo);
-
-        // Делаем свое форматирование в ISO 8601,
-        // т.к. kad.arbitr.ru "хочет" без таймзон:
-        // YYYY-MM-DDThh:mm:ss
-        //      "DateFrom": "2000-12-31T00:00:00"
-        //      "DateTo": "2014-12-01T23:59:59"
-        function toISO(d) {
-            return d.format('YYYY-MM-DDTHH:mm:ss');
-        }
-
-        return {
-            isoMonthAgo: isoMonthAgo,
-            isoYearAgo: isoYearAgo
-        };
-    }
-
-    //
-    return angular.module('np.autokad', _.pluck(submodules, 'name'))
+    return angular.module('np.autokad', _.pluck(submodules, 'name').concat(['infinite-scroll']))
         //
         .run([function(){
             template = i18n.translateTemplate(template);
         }])
         //
-        .directive('npAutokad', ['$log', '$rootScope', 'npAutokadResource', function($log, $rootScope, npAutokadResource){
+        .factory('npAutokadHelper', ['$log', '$timeout', function($log, $timeout){
+            /*
+             * request
+             *
+             */
+            var DEFAULT_SEARCH_PARAMS = {
+                name: null,
+                ogrn: null,
+                inn: null,
+                source: 'name',
+                period: 'all',
+            };
+
+            var DEFAULT_REQUEST_DATA = {
+                q: null,
+                dateFrom: null,
+                dateTo: null,
+                page: 1
+            };
+
+            var period = new Period();
+
+            var TRANSFORM_SEARCH_PARAMS = {
+                'source': {
+                    'name': function(searchParams, request) {
+                        request['q'] = searchParams['name'];
+                    },
+                    'ogrn': function(searchParams, request) {
+                        request['q'] = searchParams['ogrn'];
+                    },
+                    'inn': function(searchParams, request) {
+                        request['q'] = searchParams['inn'];
+                    }
+                },
+                'period': {
+                    'all': function(searchParams, request) {
+                        request['dateFrom'] = null;
+                    },
+                    'year': function(searchParams, request) {
+                        request['dateFrom'] = period.isoYearAgo;
+                    },
+                    'month': function(searchParams, request) {
+                        request['dateFrom'] = period.isoMonthAgo;
+                    }
+                }
+            };
+
+            function buildRequestData(searchParams) {
+                var requestData = DEFAULT_REQUEST_DATA,
+                    value;
+
+                _.each(TRANSFORM_SEARCH_PARAMS, function(paramTransform, param){
+                    value = searchParams[param];
+                    paramTransform[value](searchParams, requestData);
+                });
+
+                return requestData;
+            }
+
+            function Period() {
+                var startOfDay  = moment().startOf('day'),
+                    monthAgo    = moment(startOfDay).subtract(1, 'months'),
+                    yearAgo     = moment(startOfDay).subtract(1, 'years'),
+                    isoMonthAgo = toISO(monthAgo),
+                    isoYearAgo  = toISO(yearAgo);
+
+                // Делаем свое форматирование в ISO 8601,
+                // т.к. kad.arbitr.ru "хочет" без таймзон:
+                // YYYY-MM-DDThh:mm:ss
+                //      "DateFrom": "2000-12-31T00:00:00"
+                //      "DateTo": "2014-12-01T23:59:59"
+                function toISO(d) {
+                    return d.format('YYYY-MM-DDTHH:mm:ss');
+                }
+
+                return {
+                    isoMonthAgo: isoMonthAgo,
+                    isoYearAgo: isoYearAgo
+                };
+            }
+
+            /*
+             * loading
+             *
+             */
+            var loadingShowDelay = 500,
+                loadingId;
+
+            function loading(element, operation) {
+                loadingId = _.uniqueId();
+
+                process(loadingId, operation);
+
+                function process(id, operation) {
+                    var complete = false;
+
+                    $timeout(function(){
+                        if (!complete && id === loadingId) {
+                            element.addClass('loading');
+                        }
+                    }, loadingShowDelay);
+
+                    operation(done);
+
+                    function done() {
+                        $timeout(function(){
+                            if (id === loadingId) {
+                                element.removeClass('loading');
+                                complete = true;
+                            }
+                        });
+                    }
+                }
+            }
+
+            // API
+            return {
+                getDefaultSearchParams: function() {
+                    return DEFAULT_SEARCH_PARAMS;
+                },
+                buildRequestData: buildRequestData,
+                loading: loading
+            };
+        }])
+        //
+        .directive('npAutokad', ['$log', '$rootScope', 'npAutokadResource', 'npAutokadHelper', function($log, $rootScope, npAutokadResource, npAutokadHelper){
             return {
                 restrict: 'A',
                 template: template,
                 scope: {},
                 link: function(scope, element, attrs) {
                     var search = {
-                        params: DEFAULT_SEARCH_PARAMS,
+                        params: null,
+                        requestData: null,
                         request: null,
                         result: { // В формате kad.arbitr.ru
                             TotalCount: null,
+                            PagesCount: null,
                             Items: null
                         },
+                        pager: new Pager(),
                         getTotal: function() {
                             return search.result.TotalCount || 0;
                         },
@@ -130,11 +181,9 @@ define(function(require) {'use strict';
                         watch: true
                     };
 
-                    var kadSearchRequest;
-
                     $rootScope.$on('np-autokad-do-search', function(e, options){
                         initSearch(options.search);
-                        kadSearch(function(){
+                        doSearch(function(){
                             search.noResult = search.isEmptyResult();
                             options.success();
                         }, function(){
@@ -146,7 +195,7 @@ define(function(require) {'use strict';
                     scope.$watch('search.params', function(newValue, oldValue) {
                         if (newValue !== oldValue) {
                             if (search.watch) {
-                                kadSearch();
+                                doSearch();
                             } else {
                                 search.watch = true;
                             }
@@ -155,41 +204,113 @@ define(function(require) {'use strict';
 
                     function initSearch(params) {
                         search.watch = false;
-                        _.extend(search.params, DEFAULT_SEARCH_PARAMS, params);
+                        search.params = _.extend({}, npAutokadHelper.getDefaultSearchParams(), params);
                     }
 
-                    function kadSearch(success, error) {
-                        search.request = buildRequest(search.params);
+                    function doSearch(success, error) {
+                        search.requestData = npAutokadHelper.buildRequestData(search.params);
 
-                        $log.info('search.request...', search.request);
-
-                        var hasError = !search.request.q;
-
-                        if (hasError) {
-                            complete();
+                        if (!search.requestData.q) {
+                            complete(true);
                         } else {
-                            kadSearchRequest = npAutokadResource.kadSearch({
-                                r: search.request,
-                                previousRequest: kadSearchRequest,
-                                success: function(data, status){
-                                    search.result = data.Result;
-                                },
-                                error: function(data, status){
-                                    search.result = {};
-                                    hasError = true;
-                                }
+                            resetSearchRequest();
+                            npAutokadHelper.loading(element, function(done){
+                                searchRequest(function(hasError, result){
+                                    complete(hasError, result);
+                                    done();
+                                });
                             });
-
-                            kadSearchRequest.completePromise.then(complete);
                         }
 
-                        function complete() {
-                            if (!hasError && _.isFunction(success)) {
-                                success();
-                            } else if (hasError && _.isFunction(error)) {
+                        function complete(hasError, result) {
+                            if (!hasError) {
+                                search.result = result;
+
+                                search.pager.reset(noMore(hasError, result), function(callback){
+                                    search.requestData.page++;
+
+                                    npAutokadHelper.loading(element, function(done){
+                                        searchRequest(function(hasError, result){
+                                            _.each(result.Items, function(item){
+                                                search.result.Items.push(item);
+                                            });
+
+                                            callback(noMore(hasError, result));
+                                            done();
+                                        });
+                                    });
+                                });
+
+                                if (_.isFunction(success)) {
+                                    success();
+                                }
+                            } else if (_.isFunction(error)) {
                                 error();
                             }
                         }
+
+                        function noMore(hasError, result) {
+                            return hasError || (result ? search.requestData.page >= result.PagesCount : true);
+                        }
+                    }
+
+                    function resetSearchRequest() {
+                        if (search.request) {
+                            search.request.abort();
+                        }
+                        search.requestData.page = 1;
+                    }
+
+                    function searchRequest(callback) {
+                        var hasError    = false,
+                            result      = {};
+
+                        search.request = npAutokadResource.kadSearch({
+                            r: search.requestData,
+                            previousRequest: search.request,
+                            success: function(data, status){
+                                result = data.Result;
+                            },
+                            error: function(data, status){
+                                hasError = true;
+                            }
+                        });
+
+                        search.request.completePromise.then(complete, complete);
+
+                        function complete() {
+                            callback(hasError, result);
+                        }
+                    }
+
+                    //
+                    function Pager() {
+                        var internalDisabled    = false,
+                            noNextPage          = false,
+                            nextPageHandler     = null;
+
+                        function isDisabled() {
+                            return internalDisabled || noNextPage || !nextPageHandler;
+                        }
+
+                        return {
+                            reset: function(noMore, pageHandler) {
+                                internalDisabled = false;
+                                noNextPage = noMore;
+                                nextPageHandler = pageHandler;
+                            },
+                            nextPage: function() {
+                                if (!isDisabled() && nextPageHandler) {
+                                    internalDisabled = true;
+
+                                    nextPageHandler(function(noMore){
+                                        internalDisabled = false;
+                                        noNextPage = noMore;
+                                    });
+                                }
+                            },
+                            isDisabled: isDisabled
+                        };
                     }
 
                     //
@@ -202,7 +323,7 @@ define(function(require) {'use strict';
         //
         .filter('highlightSearch', ['$sce', function($sce){
             return function(text, query){
-                if (!query) {
+                if (!query || !text) {
                     return $sce.trustAsHtml(text);
                 }
 
