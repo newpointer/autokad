5c4e30a1f13c161e961dda1b196dfaedc4ddf03a
ankostyuk <andrew.kostyuk@gmail.com>
2016-04-13 21:24:18 +0400

    Прототип поддержки HTML-response

--- src/autokad/autokad-resource.js
+++ src/autokad/autokad-resource.js
@@ -13,8 +13,8 @@ define(function(require) {'use strict';
         //
         .constant('npAutokadResourceConfig', {
             'kad.search': {
-                // url: '/autokad/data/demo/r.json'
-                url: '/kad.arbitr.ru/Kad/SearchInstances'
+                url: '/autokad/data/demo/r.html'
+                // url: '/kad.arbitr.ru/Kad/SearchInstances'
             }
         })
         //
@@ -22,13 +22,79 @@ define(function(require) {'use strict';
 
             var kadSearchConfig = npAutokadResourceConfig['kad.search'];
 
+            //
+            var CaseTypeByClass = {
+                'administrative':   'А',
+                'civil':            'Г',
+                'bankruptcy':       'Б'
+            };
+
+            function isContentTypeHeaderAs(headers, contentType) {
+                var targetContentType = headers('Content-Type');
+
+                if (!targetContentType || !contentType) {
+                    return null;
+                }
+
+                return (targetContentType.toLowerCase()).indexOf(contentType.toLowerCase()) >= 0;
+            }
+
+            function parseKadSearchHTMLResult(htmlData) {
+                var data        = {},
+                    $htmlData   = $(htmlData),
+                    total       = parseInt($htmlData.find('#documentsTotalCount').val());
+
+                $log.info('total:', total);
+
+                if (_.isNaN(total)) {
+                    return data;
+                }
+
+                var items = [];
+
+                $htmlData.find('> tr').each(function(){
+                    var $row        = $(this),
+                        $num        = $row.find('> .num > .b-container'),
+                        $caseType   = $num.find('> div:first-child'),
+                        $caseLink   = $num.find('a'),
+                        $court      = $row.find('> .court > .b-container');
+
+                    var item = {
+                        "CaseId":       _.last(_.words($caseLink.attr('href'), '/')),
+                        "CaseNumber":   _.trim($caseLink.text()),
+                        "CaseType":     CaseTypeByClass[$caseType.attr('class')],
+                        "CourtName":    _.trim($court.find('> div:last-child').text()),
+                        "Judge":        _.trim($court.find('.judge').text()),
+                        "Date":         _.trim($caseType.find('> span').text())
+                    };
+
+                    $log.info('item:', item);
+
+                    items.push(item);
+                });
+
+                var result = data['Result'] = {
+                    "Page":         parseInt($htmlData.find('#documentsPage').val()),
+                    "PageSize":     parseInt($htmlData.find('#documentsPageSize').val()),
+                    "PagesCount":   parseInt($htmlData.find('#documentsPagesCount').val()),
+                    "TotalCount":   total,
+                    "Items":        items
+                };
+
+                function getCaseTypeByClass() {
+
+                }
+
+                return data;
+            }
+
             // API
             return {
                 kadSearch: function(options) {
                     // docs/autokad.md#Поиск арбитражных дел
                     return npResource.request({
-                        // method: 'GET',
-                        method: 'POST',
+                        method: 'GET',
+                        // method: 'POST',
                         url: kadSearchConfig.url,
                         data: {
                             "Sides": [{
@@ -47,14 +113,21 @@ define(function(require) {'use strict';
                             "WithVKSInstances": false
                         },
                         headers: {
-                            "Accept": 'application/json, text/javascript, */*',
+                            "Accept": '*/*',
                             "Content-Type": 'application/json',
                             "x-date-format": 'iso',
                             "X-Requested-With": 'XMLHttpRequest'
                         }
                     }, {
-                        responseProcess: function(data) {
-                            if (_.isEmpty(data.Result)) {
+                        responseProcess: function(data, status, headers, config) {
+                            $log.info('success...', headers('Content-Type'));
+
+                            if (isContentTypeHeaderAs(headers, 'text/html')) {
+                                $log.warn('HTML...');
+                                data = parseKadSearchHTMLResult(data);
+                            }
+
+                            if (_.isEmpty(data['Result'])) {
                                 $log.warn('kadSearch... <Result> is empty:', data);
                             }
                             return data;
