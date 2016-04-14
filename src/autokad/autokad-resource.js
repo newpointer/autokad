73f4d49d17af7386d1310c5a9771ca33f6173d4a
ankostyuk <andrew.kostyuk@gmail.com>
2016-04-14 14:42:33 +0300

    Resolve #19: Поддержка HTML-результата от kad.arbitr.ru

--- src/autokad/autokad-resource.js
+++ src/autokad/autokad-resource.js
@@ -13,8 +13,11 @@ define(function(require) {'use strict';
         //
         .constant('npAutokadResourceConfig', {
             'kad.search': {
-                url: '/autokad/data/demo/r.html'
-                // url: '/kad.arbitr.ru/Kad/SearchInstances'
+                // url: '/autokad/data/demo/r.html'
+                // url: '/autokad/data/demo/r-empty.html'
+                // url: '/autokad/data/demo/r-fail-1.html'
+                // url: '/autokad/data/demo/r-fail-2.html'
+                url: '/kad.arbitr.ru/Kad/SearchInstances'
             }
         })
         //
@@ -40,11 +43,12 @@ define(function(require) {'use strict';
             }
 
             function parseKadSearchHTMLResult(htmlData) {
-                var data        = {},
-                    $htmlData   = $(htmlData),
-                    total       = parseInt($htmlData.find('#documentsTotalCount').val());
+                var $htmlData = $('<div>', {
+                    html: htmlData
+                });
 
-                $log.info('total:', total);
+                var data        = {},
+                    total       = parseInt($htmlData.find('#documentsTotalCount').val());
 
                 if (_.isNaN(total)) {
                     return data;
@@ -52,38 +56,60 @@ define(function(require) {'use strict';
 
                 var items = [];
 
-                $htmlData.find('> tr').each(function(){
-                    var $row        = $(this),
-                        $num        = $row.find('> .num > .b-container'),
-                        $caseType   = $num.find('> div:first-child'),
-                        $caseLink   = $num.find('a'),
-                        $court      = $row.find('> .court > .b-container');
+                $htmlData.find('tr').each(function(){
+                    var $row            = $(this),
+                        $num            = $row.find('> .num > .b-container'),
+                        $caseType       = $num.find('> div:first-child'),
+                        $caseLink       = $num.find('a'),
+                        $court          = $row.find('> .court > .b-container'),
+                        plaintiffCount  = parseInt(_.clean($row.find('.plaintiff .b-button-container .b-button strong').text())),
+                        respondentCount = parseInt(_.clean($row.find('.respondent .b-button-container .b-button strong').text())),
+                        plaintiffs      = [],
+                        respondents     = [];
+
+                    $row.find('.plaintiff .js-rolloverHtml').each(function(){
+                        plaintiffs.push(buildParticipant($(this)));
+                    });
+
+                    $row.find('.respondent .js-rolloverHtml').each(function(){
+                        respondents.push(buildParticipant($(this)));
+                    });
 
                     var item = {
                         "CaseId":       _.last(_.words($caseLink.attr('href'), '/')),
-                        "CaseNumber":   _.trim($caseLink.text()),
+                        "CaseNumber":   _.clean($caseLink.text()),
                         "CaseType":     CaseTypeByClass[$caseType.attr('class')],
-                        "CourtName":    _.trim($court.find('> div:last-child').text()),
-                        "Judge":        _.trim($court.find('.judge').text()),
-                        "Date":         _.trim($caseType.find('> span').text())
+                        "CourtName":    _.clean($court.find('> div:last-child').text()),
+                        "Judge":        _.clean($court.find('.judge').text()),
+                        "Date":         _.clean($caseType.find('> span').text()),
+                        "Plaintiffs":   {
+                            "Count": plaintiffCount || _.size(plaintiffs),
+                            "Participants": plaintiffs
+                        },
+                        "Respondents":  {
+                            "Count": respondentCount || _.size(respondents),
+                            "Participants": respondents
+                        }
                     };
 
-                    $log.info('item:', item);
+                    function buildParticipant($p) {
+                        return {
+                            "Name":     _.clean($p.find('strong').text()),
+                            "Inn":      _.clean(_.replaceAll($p.find('div:contains("ИНН")').text(), 'ИНН:', '')),
+                            "Address":  _.clean($p.clone().children().remove().end().text())
+                        };
+                    }
 
                     items.push(item);
                 });
 
-                data['Result'] = {
-                    "Page":         parseInt($htmlData.find('#documentsPage').val()),
-                    "PageSize":     parseInt($htmlData.find('#documentsPageSize').val()),
-                    "PagesCount":   parseInt($htmlData.find('#documentsPagesCount').val()),
+                data["Result"] = {
+                    "Page":         parseInt($htmlData.find('#documentsPage').val()) || null,
+                    "PageSize":     parseInt($htmlData.find('#documentsPageSize').val()) || null,
+                    "PagesCount":   parseInt($htmlData.find('#documentsPagesCount').val()) || null,
                     "TotalCount":   total,
                     "Items":        items
                 };
-
-                function getCaseTypeByClass() {
-
-                }
 
                 return data;
             }
@@ -93,8 +119,8 @@ define(function(require) {'use strict';
                 kadSearch: function(options) {
                     // docs/autokad.md#Поиск арбитражных дел
                     return npResource.request({
-                        method: 'GET',
-                        // method: 'POST',
+                        // method: 'GET',
+                        method: 'POST',
                         url: kadSearchConfig.url,
                         data: {
                             "Sides": [{
@@ -120,14 +146,11 @@ define(function(require) {'use strict';
                         }
                     }, {
                         responseProcess: function(data, status, headers, config) {
-                            $log.info('success...', headers('Content-Type'));
-
                             if (isContentTypeHeaderAs(headers, 'text/html')) {
-                                $log.warn('HTML...');
                                 data = parseKadSearchHTMLResult(data);
                             }
 
-                            if (_.isEmpty(data['Result'])) {
+                            if (_.isEmpty(data["Result"])) {
                                 $log.warn('kadSearch... <Result> is empty:', data);
                             }
                             return data;
